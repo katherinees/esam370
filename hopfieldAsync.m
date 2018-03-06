d15a121480f074c10357c0eca1843f3c8942e89b
Ethan Rucinski <ethan.rucinski@gmail.com>
2018-03-05 21:04:04 -0600

    Finished hopfield simulator

--- hopfieldAsync.m
+++ hopfieldAsync.m
@@ -1,8 +1,7 @@
-function v_i = hopfieldAsync(v, M, n)
-
+function v_i = hopfieldAsync(v, M)
+n = ceil(numel(v)*rand);
 v_i_n = M(n,:) * v;
-
 v_i = v;
-v_i(n) = v_i_n;
+v_i(n) = sign(v_i_n + (10^-10)*(2*rand-1));
 
 end
